98f3357acf0c8d5b007407ef100b21e551bca67f
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-06-09 17:42:46 +0200

    start config

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const { router } = require('./controllers/home');
+
+const app = express();
+const hbs = handlebars.create({
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(router);
+
+app.listen(3000, () => console.log('Server started http://localhost:3000'));
